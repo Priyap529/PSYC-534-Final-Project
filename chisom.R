ec133847052ac67ed700776903bebbeb3b8c5dd3
somapsych <deborahu@live.unc.edu>
2025-04-18 16:06:51 -0400

    Filtering Drug and Placebo Data
    combined levels in the study factor into a new data set to work from

--- chisom.R
+++ chisom.R
@@ -1,7 +1,25 @@
 #this is in class activity - chisom
 
-install.packages("psychTools")
-
+install.packages(c("dplyr", "lattice", "psych", "psychTools"))
 library(psychTools)
+library(psych)
+library(dplyr)
+library(lattice)
+
 data(sai)
 force(sai)
+
+# MERGE DATA INTO CAFFEINE VARIABLE = DRUG AND CONTROL/NORESPONSE GROUPS = PLACEBO
+
+sai <- mutate(sai,grouped_study = recode(study,"AGES" = "DRUG","CITY"="DRUG","EMIT"="DRUG","SALT"="DRUG","XRAY"="DRUG","Cart" = "PLACEBO","Fast"="PLACEBO","Shed"="PLACEBO", "Raft"="PLACEBO","Shop"="PLACEBO"))
+
+#FILTER DRUG AND PLACEBO VARIABLE INTO NEW VECTOR
+
+sai_filtered <- filter(sai, grouped_study %in% c("DRUG", "PLACEBO"))
+
+# median differences
+
+# figuring out what new function in CRAN to introduce in our code
+
+
+
